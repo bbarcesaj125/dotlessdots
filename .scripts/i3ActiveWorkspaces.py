5f7b90bda8dd4c90a8b6b6b2e32f06a076ad4417
Yusuf Felly <beebarss.saj.ma@gmail.com>
2019-01-18 19:50:47 +0100

    Added a separated workspace for Firefox and changed the workspace script accordingly.

--- .scripts/i3ActiveWorkspaces.py
+++ .scripts/i3ActiveWorkspaces.py
@@ -1,10 +1,17 @@
 import sys, json
 
+""" 
+This is a Python script that works as an i3 workspace indicator.
+It uses an icon font (e.g., Awesome) to output a string of icons that change depending 
+on the workspaces' status.
+It should be used with a bash script wrapper that feeds it the list of workspaces using the i3 command `i3-msg`.
+"""
+
 for line in sys.stdin:
     #print (line)
     data = json.loads(line)
-    d = {"α: www": 0, "β: zim": 1, "γ: video": 2, "δ: manga": 3, "ε: term": 4, "ζ: fm": 5, "η: gimp": 6, "θ: misc": 7}
-    e = [ "", "", "", "", "", "", "", "" ]
+    d = {"α: www": 0, "β: www1": 1, "γ: zim": 2, "δ: video": 3, "ε: manga": 4, "ζ: term": 5, "η: fm": 6, "θ: gimp": 7, "ι: misc": 8}
+    e = ["", "", "", "", "", "", "", "", ""]
     y = []
     s = []
     for i, name in enumerate(k['name'] for k in data):
@@ -17,7 +24,6 @@ for line in sys.stdin:
         if focused == True:
             dd = data[i]['name']
             #print(dd)
-
             kk = d[dd]
             #print(kk)
             e[kk] = ""
